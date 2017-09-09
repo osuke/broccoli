04639c143d6e7c14660dafe2f03d6ca1822c6603
osuke <funxion.mail@gmail.com>
2017-09-09 17:16:35 +0900

    modified styles of the button in header

--- src/components/ModalWebview.js
+++ src/components/ModalWebview.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Modal, View, WebView, Text, TouchableOpacity } from 'react-native'
+import { Modal, View, WebView, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { EvilIcons } from '@expo/vector-icons'
 
 export default class ModalWebview extends React.Component {
   render () {
@@ -9,11 +10,17 @@ export default class ModalWebview extends React.Component {
         transparent={false}
         visible={this.props.webview.visible}
       >
-        <View style={{marginTop: 20}}>
+        <View style={styles.header}>
           <TouchableOpacity
             onPress={ () => { this.props.hidePage() } }
+            style={styles.backBtn}
           >
-            <Text>戻る</Text>
+            <EvilIcons
+              name="chevron-left"
+              size={44}
+              color="#a0a0a0"
+              style={styles.icon}
+            />
           </TouchableOpacity>
         </View>
         <WebView source={{uri: this.props.webview.url}} />
@@ -21,3 +28,20 @@ export default class ModalWebview extends React.Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 20,
+    height: 46,
+    borderBottomColor: '#e6e6e6',
+    borderBottomWidth: 1,
+  },
+  backBtn: {
+    width: 45,
+    backgroundColor: 'transparent'
+  },
+  icon: {
+    marginTop: 5,
+    marginLeft: 0,
+  }
+})
