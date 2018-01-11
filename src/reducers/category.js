0a8c64456389765a3589f146e8aa6894efba819e
osuke <funxion.mail@gmail.com>
2018-01-11 23:39:03 +0900

    modified category reducer

--- src/reducers/category.js
+++ src/reducers/category.js
@@ -1,6 +1,74 @@
-import { FETCH_ARTICLES, FETCH_FAV_ARTICLES, FETCH_BOOKMARK_ARTICLES, CLEAR_ARTICLES } from '../actions/fetchArticles'
+import {
+  FETCH_ARTICLES,
+  FETCH_FAV_ARTICLES,
+  FETCH_BOOKMARK_ARTICLES,
+  CLEAR_ARTICLES
+} from '../actions/fetchArticles'
 
 const initialState = {
+  items: {
+    hotentry: {
+      name: '総合',
+      url: 'http://b.hatena.ne.jp/hotentry.rss',
+      items: []
+    },
+    general: {
+      name: '一般',
+      url: 'http://b.hatena.ne.jp/hotentry/general.rss',
+      items: []
+    },
+    social: {
+      name: '世の中',
+      url: 'http://b.hatena.ne.jp/hotentry/social.rss',
+      items: []
+    },
+    economics: {
+      name: '政治と経済',
+      url: 'http://b.hatena.ne.jp/hotentry/economics.rss',
+      items: []
+    },
+    life: {
+      name: '暮らし',
+      url: 'http://b.hatena.ne.jp/hotentry/life.rss',
+      items: []
+    },
+    knowledge: {
+      name: '学び',
+      url: 'http://b.hatena.ne.jp/hotentry/knowledge.rss',
+      items: []
+    },
+    it: {
+      name: 'テクノロジー',
+      url: 'http://b.hatena.ne.jp/hotentry/it.rss',
+      items: []
+    },
+    fun: {
+      name: 'おもしろ',
+      url: 'http://b.hatena.ne.jp/hotentry/fun.rss',
+      items: []
+    },
+    entertainment: {
+      name: 'エンタメ',
+      url: 'http://b.hatena.ne.jp/hotentry/entertainment.rss',
+      items: []
+    },
+    game: {
+      name: 'アニメとゲーム',
+      url: 'http://b.hatena.ne.jp/hotentry/game.rss',
+      items: []
+    },
+    myBookmark: {
+      name: 'マイブックマーク',
+      items: [],
+      offset: 0
+    },
+    fav: {
+      name: 'お気に入り',
+      items: [],
+      offset: 0
+    },
+  }
+/*
   items: [
     {
       name: '総合',
@@ -63,6 +131,7 @@ const initialState = {
       offset: 0
     }
   ]
+*/
 }
 
 export default (state = initialState, action) => {
