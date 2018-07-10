3dfee4cfd786147c080ded199ca624afd3ebe182
osuke <funxion.mail@gmail.com>
2018-07-11 01:14:57 +0900

    design change for bookmark form

--- src/components/BookmarkForm.js
+++ src/components/BookmarkForm.js
@@ -71,19 +71,43 @@ export default class BookmarkForm extends Component {
         </Header>
         <ScrollView
           style={styles.content}
-          keyboardShouldPersistTaps="always"
-          scrollEnabled={false}
         >
-          <Input
-            autoFocus
-            autoCorrect={false}
-            multiline = {true}
-            numberOfLines = {6}
-            style={[styles.comment, {height: this.state.inputHeight}]}
-            onChangeText={(text) => { this.setState({text})}}
-            value={this.state.text}
-          />
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', backgroundColor: '#fff' }}>
+          <View style={styles.sec}>
+            <View style={styles.title}>
+              <Text style={styles.titleText}>コメント</Text>
+            </View>
+            <Input
+              autoFocus
+              autoCorrect={false}
+              multiline={true}
+              numberOfLines={6}
+              style={styles.comment}
+              onChangeText={(text) => { this.setState({text})}}
+              value={this.state.text}
+              placeholder="コメントを追加"
+              placeholderTextColor="#999999"
+            />
+          </View>
+
+          <View style={styles.sec}>
+            <View style={styles.title}>
+              <Text style={styles.titleText}>タグ</Text>
+            </View>
+            <View style={styles.tagSec}>
+              <View style={styles.tagInput}>
+                <Input
+                  style={styles.tagInputField}
+                  placeholder="タグを追加"
+                  placeholderTextColor="#999999"
+                />
+              </View>
+              <View style={styles.tagList}>
+                <Button style={styles.tag}><Text style={styles.tagText}>あとで読む</Text></Button>
+                <Button style={styles.tag}><Text style={styles.tagText}>デザイン</Text></Button>
+              </View>
+            </View>
+          </View>
+          <View style={styles.submitArea}>
             <View>
               <Button
                 onPress={() => {
@@ -123,7 +147,20 @@ export default class BookmarkForm extends Component {
 const styles = StyleSheet.create({
   content: {
     flex: 1,
-    backgroundColor: '#efefef'
+    backgroundColor: '#efefef',
+    paddingTop: 20,
+  },
+  sec: {
+    marginBottom: 16,
+  },
+  title: {
+    marginBottom: 8,
+    paddingLeft: 12,
+  },
+  titleText: {
+    fontSize: 14,
+    color: '#6B6B6B',
+    fontWeight: 'bold',
   },
   comment: {
     borderColor: 'gray',
@@ -133,7 +170,9 @@ const styles = StyleSheet.create({
     paddingTop: 12,
     paddingRight: 12,
     paddingLeft: 12,
-    paddingBottom: 12
+    paddingBottom: 12,
+    fontSize: 16,
+    height: 160,
   },
   header: {
     height: 65,
@@ -153,17 +192,57 @@ const styles = StyleSheet.create({
     marginTop: 28
   },
   btn: {
-    borderLeftWidth: 1,
-    borderLeftColor: '#e5e5e5',
-    borderRadius: 0,
+    borderRadius: 23,
+    width: 196,
+    height: 46,
+    backgroundColor: '#3dc264',
+    flex: 1,
+    justifyContent: 'center',
   },
   btnText: {
-    color: '#222',
-    fontSize: 14,
-    marginTop: -4
+    color: '#fff',
+    fontSize: 18,
+    marginTop: -4,
+    lineHeight: 46,
   },
   deleteButtonText: {
     color: '#fff'
+  },
+  tagSec: {
+    backgroundColor: '#fff',
+    paddingLeft: 12,
+  },
+  tagInputField: {
+    paddingLeft: 0,
+    paddingTop: 12,
+    paddingRight: 12,
+    paddingBottom: 12,
+    fontSize: 16,
+  },
+  tagList: {
+    borderTopWidth: 1,
+    borderTopColor: '#d1d1d1',
+    paddingTop: 12,
+    paddingBottom: 12,
+    flex: 1,
+    flexDirection: 'row',
+  },
+  tag: {
+    borderRadius: 23,
+    height: 30,
+    backgroundColor: '#dbdbdb',
+    marginRight: 8,
+  },
+  tagText: {
+    lineHeight: 16,
+    color: '#6b6b6b',
+    fontSize: 16,
+  },
+  submitArea: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
   }
 })
 
