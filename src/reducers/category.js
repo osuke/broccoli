3c443ffca92cd16512af3d7971b021ad216692a8
osuke <funxion.mail@gmail.com>
2018-08-22 00:09:59 +0900

    to work refresh correctly on mybookmark and fav

--- src/reducers/category.js
+++ src/reducers/category.js
@@ -88,7 +88,15 @@ export default (state = initialState, action) => {
       }
       return Object.assign({}, state, newState)
     case FETCH_FAV_ARTICLES:
-      newState.items[action.payload.index].items = [...action.payload.item]
+
+      // after refresh
+      if (action.payload.offset === 25) {
+        newState.items[action.payload.index].items = [...action.payload.item]
+
+      // after reaching end
+      } else {
+        newState.items[action.payload.index].items = [...newState.items[action.payload.index].items, ...action.payload.item]
+      }
       newState.items[action.payload.index].offset = action.payload.offset
       return Object.assign({}, state, newState)
     case FETCH_BOOKMARK_ARTICLES:
@@ -97,7 +105,15 @@ export default (state = initialState, action) => {
         obj.domain = domain
         obj.favicon = `https://www.google.com/s2/favicons?domain=${domain}`
       })
-      newState.items[action.payload.index].items = [...action.payload.item]
+
+      // after refresh
+      if (action.payload.offset === 20) {
+        newState.items[action.payload.index].items = [...action.payload.item]
+
+      // after reaching end
+      } else {
+        newState.items[action.payload.index].items = [...newState.items[action.payload.index].items, ...action.payload.item]
+      }
       newState.items[action.payload.index].offset = action.payload.offset
       return Object.assign({}, state, newState)
     default:
